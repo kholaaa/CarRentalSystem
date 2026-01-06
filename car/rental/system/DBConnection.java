c059a51a06802b353206246d63f63de42d5fc047
Khola Asghar <kholaasghar20@gmail.com>
2026-01-07 02:14:25 +0500

    Revert "DATABASE CONNECTIVITY"
    This reverts commit 758c9c335a9cf0366dc80ca0daf256c0333f1dec.
    
    :wq
    :wq

--- car/rental/system/DBConnection.java
+++ car/rental/system/DBConnection.java
@@ -4,23 +4,12 @@ import java.sql.*;
 
 public class DBConnection {
 
-    static final String URL = "jdbc:mysql://localhost:3306/CarRental?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    // Change your DB URL, user, password as needed
+    static final String URL = "root@127.0.0.1:3306";
     static final String USER = "root";
     static final String PASSWORD = "ot10forever10@";
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        // Load the MySQL driver (important!)
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-
-    public static void main(String[] args) {
-        try (Connection conn = getConnection()) {
-            if (conn != null) {
-                System.out.println("Connection successful!");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
